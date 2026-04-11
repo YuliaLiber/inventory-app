274a94b64f8e8a56c5220c8e359068d473b89744
YuliaLiber <liberyula07@gmail.com>
2026-04-10 19:01:57 -0700

    add routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import AdminInventory from "./pages/AdminInventory";
+import AdminInventoryCreate from "./pages/AdminInventoryCreate";
+import AdminInventoryEdit from "./pages/AdminInventoryEdit";
+import AdminInventoryDetails from "./pages/AdminInventoryDetails";
+
 function App() {
   return (
-    <div>
-      <h1>Inventory Admin Panel</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<AdminInventory />} />
+      <Route path="/create" element={<AdminInventoryCreate />} />
+      <Route path="/edit/:id" element={<AdminInventoryEdit />} />
+      <Route path="/details/:id" element={<AdminInventoryDetails />} />
+    </Routes>
   );
 }
 
