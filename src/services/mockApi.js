ffb426f7594c3d83c32c06cdb8ff716b9307e0fa
YuliaLiber <liberyula07@gmail.com>
2026-04-21 15:14:27 -0700

    improve admin table with description, photo and actions

--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -3,15 +3,39 @@ const BASE_URL = "https://api.example.com/inventory";
 let inventory = [
   {
     id: 1,
-    inventory_name: "Laptop",
-    description: "Powerful laptop",
-    photo: "https://via.placeholder.com/100",
+    inventory_name: "Laptop Pro",
+    description: "Dell Pro Max 16 Laptop",
+    photo: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/dell-plus/db14255/laptop-db14255t-copilot-pc-mg.png?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1179&qlt=100,1&resMode=sharp2&size=1179,804&chrss=full",
   },
   {
     id: 2,
     inventory_name: "Phone",
-    description: "Smartphone",
-    photo: "https://via.placeholder.com/100",
+    description: "Apple iPhone 17 Pro 512GB (Cosmic Orange)",
+    photo: "https://smartbuy-me.com/cdn/shop/files/ABJ1401ST0049.jpg?v=1759585497&width=800",
+  },
+  {
+    id: 3,
+    inventory_name: "Laptop",
+    description: "Dell 14 Plus Laptop",
+    photo: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/dell/dell-15-intel-3530/media-gallery/laptop-dell-dc15250nt-bk-plastic-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1254&qlt=100,1&resMode=sharp2&size=1254,804&chrss=full",
+  },
+  {
+    id: 4,
+    inventory_name: "Phone Max",
+    description: "Apple iPhone 17 Pro Max 256GB (Silver)",
+    photo: "https://my-apple.com.ua/image/catalog/products/iphone/iphone-17-pro-max/iphone-17-pro-max-silver-1.png",
+  },
+  {
+    id: 5,
+    inventory_name: "Headphones",
+    description: "Apple AirPods 4 (2024)",
+    photo: "https://applehome.te.ua/wp-content/uploads/2024/09/42221.750.jpeg",
+  },
+  {
+    id: 6,
+    inventory_name: "Headphones Max",
+    description: "Apple AirPods Max USB-C 2024 (Midnight)",
+    photo: "https://www.mrfix.ua/storage/66f/172/db9/product-product-apple-airpods-max-usb-c-midnight-mww43-32843-500x500.webp",
   },
 ];
 
